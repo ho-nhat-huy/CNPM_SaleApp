8b668d4c88bb7db3c7a8b83568b1e1de6ceb4f9f
nhat ha <nhathuypt1303@gmail.com>
2025-10-22 22:10:46 +0700

    updated

--- saleapp/dao.py
+++ saleapp/dao.py
@@ -2,17 +2,33 @@ import json
 
 
 def load_categories():
-    with open("data/category.json", encoding="Utf-8") as f:
+    with open("data/category.json", encoding="utf-8") as f:
         return json.load(f)
 
-def load_products(q=None):
-    with open("data/product.json", encoding="Utf-8") as f:
+
+def load_products(q=None, cate_id=None):
+    with open("data/product.json", encoding="utf-8") as f:
         products = json.load(f)
 
         if q:
-            products = [p for p in products if p["name"].find(q)>=0]
+            products = [p for p in products if p["name"].find(q) >= 0]
+
+        if cate_id:
+            products = [p for p in products if p["cate_id"].__eq__(int(cate_id))]
 
         return products
 
-if __name__=="__main__":
+
+def get_product_by_id(id):
+    with open("data/product.json", encoding="utf-8") as f:
+        products = json.load(f)
+
+        for p in products:
+            if p['id'].__eq__(id):
+                return p
+
+    return None
+
+
+if __name__ == "__main__":
     print(load_products("Iphone"))
--- saleapp/index.py
+++ saleapp/index.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template, request
-from panel.io.notebook import render_template
 import dao
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     q = request.args.get("q")
-    print(q)
-    cates = dao.load_categories()
-    prods = dao.load_products(q=q)
-    return render_template("index.html", cates=cates, prods=prods)
+    cate_id = request.args.get("cate_id")
+    prods = dao.load_products(q=q, cate_id=cate_id)
+    return render_template("index.html", prods=prods)
 
-if __name__=="__main__":
+
+@app.route("/products/<int:id>")
+def details(id):
+    return render_template("product-details.html", prod=dao.get_product_by_id(id))
+
+
+@app.context_processor
+def common_attribute():
+    return {
+        "cates": dao.load_categories()
+    }
+
+
+if __name__ == "__main__":
     with app.app_context():
         app.run(debug=True)
